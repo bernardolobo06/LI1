0df07cec50c77ab087b7722eb30a9442d452df60
bernardolobo06 <marcosbernardolobo2006@gmail.com>
2024-10-09 21:25:44 +0100

    Finished exercise 4 and exported haddock

--- Ficha3/Ficha3.hs
+++ Ficha3/Ficha3.hs
@@ -14,7 +14,7 @@ module Ficha3 where
 
 {-| A função 'remove' recebe uma lista de strings e remove todas as strings iniciadas por um dado caractere.
 
-== __Notas:__
+==  __Notas:__
 * Esta função é sensível a maiúsculas e minúsculas;
 * Esta função funciona para caracteres numéricos;
 * Esta função funciona para caracteres especiais;
@@ -31,10 +31,7 @@ module Ficha3 where
 == __Propriedades:__
 prop> remove [] _ = []
 -}
-remove ::
-  [String]      -- ^ argumento
-  -> Char       -- ^ argumento
-  -> [String]   -- ^ resultado
+remove :: [String] -> Char -> [String]  -- ^ Exercício 1
 remove [] _ = []
 remove (h:t) char
     | head h /= char = h : remove t char
@@ -56,10 +53,7 @@ remove (h:t) char
 == __Propriedades:__
 prop> addTuple [] _ = []
 -}
-addTuple ::
-  [(Int, Int)]      -- ^ argumento
-  -> Int            -- ^ argumento
-  -> [(Int, Int)]   -- ^ resultado
+addTuple :: [(Int, Int)] -> Int -> [(Int, Int)]
 addTuple [] _ = []
 addTuple (h:t) num = (fst h + num, snd h) : addTuple t num
 
@@ -75,39 +69,155 @@ addTuple (h:t) num = (fst h + num, snd h) : addTuple t num
 == __Propriedades:__
 prop> maxSndTuple [(a, b)] = b
 -}
-maxSndTuple ::
-  [(Int, Int)]      -- ^ argumento (lista não vazia)
-  -> Int            -- ^ resultado
+maxSndTuple :: [(Int, Int)] -> Int 
 maxSndTuple [(a, b)] = b
 maxSndTuple (h:t) = max (snd h) (maxSndTuple t)
 
 
------continuar a escrever a DOCUMENTAÇÃO a partir deste ponto-----
+-- | O @type@ 'Nome' é sinónimo de uma @string@ (representativa de uma identidade).
+type Nome = String             
+-- | O @type@ 'Coordenada' é sinónimo de um par de @doubles@ (representativo de um conjunto de coordenadas cartesianas).       
+type Coordenada = (Double, Double)                           
+-- | O @data type@ 'Movimento' é a definição de um novo tipo de dados (representativo de um movimento segundo os pontos cardeais).     
+data Movimento = N  -- ^(North) --> y + 1
+               | S  -- ^(South) --> y - 1
+               | E  -- ^(East) --> x + 1
+               | W  -- ^(West) --> x - 1
+              deriving (Show,Eq)   
+-- | O @type@ 'Movimentos' é sinónimo de uma lista do tipo 'Movimento' (representativa de uma sequência finita de movimentos).       
+type Movimentos = [Movimento]               
+-- | O @data type@ 'PosicaoPessoa' é a definição de um novo tipo de dados (representativo de um conjunto de coordenadas cartesianas referentes a uma identidade).               
+data PosicaoPessoa = Pos Nome Coordenada deriving (Show,Eq)     
 
-type Nome = String
-type Coordenada = (Double, Double)
-data Movimento = N | S | E | W deriving (Show,Eq) -- Norte, Sul, Este, Oeste
-type Movimentos = [Movimento]
-data PosicaoPessoa = Pos Nome Coordenada deriving (Show,Eq)
 
--------exercicio extra: criar funcao auxiliar para executar os movimentos, ao invés de fazer por casos--------
+{-| A função 'auxCoord' recebe um @data type@ 'PosicaoPessoa' e um 'Movimento' e calcula novas coordenadas.
 
---assume-se que o movimento é de uma unidade
+==  __Exemplos de utilização:__
+>>> auxCoord (Pos "Afonso" (0, 0)) N
+(Pos "Afonso" (0, 1))
+>>> auxCoord (Pos "Afonso" (0, 0)) S
+(Pos "Afonso" (0, -1))
+>>> auxCoord (Pos "Afonso" (0, 0)) E
+(Pos "Afonso" (1, 0))
+>>> auxCoord (Pos "Afonso" (0, 0)) W
+(Pos "Afonso" (-1, 0))
+-}
+auxCoord :: PosicaoPessoa -> Movimento -> PosicaoPessoa
+auxCoord (Pos name (x, y)) N = Pos name (x, y + 1)
+auxCoord (Pos name (x, y)) S = Pos name (x, y - 1)
+auxCoord (Pos name (x, y)) E = Pos name (x + 1, y)
+auxCoord (Pos name (x, y)) W = Pos name (x - 1, y)
+
+
+{-| A função 'posicoesM' recebe uma lista de posições de pessoas e um movimento e atualiza as coordenadas de todas as posições.
+
+==  __Notas:__
+* Esta função recorre à anterioremente definida 'auxCoord'.
+* Assume-se que o movimento corresponde a uma unidade do referencial.
+
+==  __Exemplos de utilização:__
+>>> posicoesM [(Pos "Afonso" (0, 0)), (Pos "Bernardo" (5, 3))] N
+[(Pos "Afonso" (0, 1)), (Pos "Bernardo" (5, 4))]
+>>> posicoesM [(Pos "Afonso" (0, 0)), (Pos "Bernardo" (5, 3))] S
+[(Pos "Afonso" (0, -1)), (Pos "Bernardo" (5, 2))]
+>>> posicoesM [(Pos "Afonso" (0, 0)), (Pos "Bernardo" (5, 3))] E
+[(Pos "Afonso" (1, 0)), (Pos "Bernardo" (6, 3))]
+>>> posicoesM [(Pos "Afonso" (0, 0)), (Pos "Bernardo" (5, 3))] W
+[(Pos "Afonso" (-1, 0)), (Pos "Bernardo" (4, 3))]
+
+== __Propriedades:__
+prop> posicoesM [] _ = []
+-}
 posicoesM :: [PosicaoPessoa] -> Movimento -> [PosicaoPessoa]
 posicoesM [] _ = []
-posicoesM ((Pos name (x, y)):t) N = Pos name (x, y+1) : posicoesM t N
-posicoesM ((Pos name (x, y)):t) S = Pos name (x, y-1) : posicoesM t S
-posicoesM ((Pos name (x, y)):t) E = Pos name (x+1, y) : posicoesM t E
-posicoesM ((Pos name (x, y)):t) W = Pos name (x-1, y) : posicoesM t W
+posicoesM (h:t) mov = auxCoord h mov : posicoesM t mov
 
+
+{-| A função 'posicao' recebe um @data type@ 'PosicaoPessoa' e uma lista de movimentos e calcula a posição da pessoa depois de executar todos os movimentos.
+
+==  __Notas:__
+* Esta função recorre à anterioremente definida 'auxCoord'.
+* Assume-se que o movimento corresponde a uma unidade do referencial.
+
+==  __Exemplos de utilização:__
+>>> posicao (Pos "Afonso" (0, 0)) [S, S, W]
+Pos "Afonso" (-1, -2)
+>>> posicao (Pos "Afonso" (0, 0)) [N, N, E, S]
+Pos "Afonso" (1, 1)
+
+== __Propriedades:__
+prop> posicao pos [] = pos
+-}
 posicao :: PosicaoPessoa -> Movimentos -> PosicaoPessoa
 posicao pos [] = pos
-posicao (Pos name (x, y)) (h:t)
-  | h == N = posicao (Pos name (x, y+1)) t
-  | h == S = posicao (Pos name (x, y-1)) t
-  | h == E = posicao (Pos name (x+1, y)) t
-  | h == W = posicao (Pos name (x-1, y)) t
+posicao pos (h:t) = posicao (auxCoord pos h) t
 
+
+{-| A função 'posicoesMs' recebe uma lista de posições de pessoas e uma lista de movimentos e calcula a posição de cada pessoa depois de executar todos os movimentos.
+
+==  __Notas:__
+* Esta função recorre à anterioremente definida 'auxCoord'.
+* Esta função recorre à anterioremente definida 'posicao'.
+* Assume-se que o movimento corresponde a uma unidade do referencial.
+
+==  __Exemplos de utilização:__
+>>> posicoesMs [(Pos "Afonso" (0, 0)), (Pos "Bernardo" (5, -5))] [S, S, W]
+[(Pos "Afonso" (-1, -2)), (Pos "Bernardo" (4, -7))]
+>>> posicao [(Pos "Afonso" (0, 0)), (Pos "Bernardo" (5, -5))] [N, N, E, S]
+[(Pos "Afonso" (1, 1)), (Pos "Bernardo" (6, -4))]
+
+== __Propriedades:__
+prop> posicoesMs [] _ = []
+-}
 posicoesMs :: [PosicaoPessoa] -> Movimentos -> [PosicaoPessoa]
 posicoesMs [] _ = []
 posicoesMs (h:t) mov = posicao h mov : posicoesMs t mov
+
+
+{-| A função 'pessoaNorte' recebe uma lista de posições de pessoas e indica qual a posição mais a Norte.
+
+==  __Notas:__
+* Assume-se que a pessoa mais a Norte é aquela cuja ordenada é maior.
+
+==  __Exemplos de utilização:__
+>>> pessoaNorte [(Pos "Afonso" (0, 0)), (Pos "Bernardo" (5, -5))]
+Just (Pos "Afonso" (0, 0))
+>>> pessoaNorte [(Pos "Afonso" (0, 0)), (Pos "Bernardo" (5, 5))]
+Just (Pos "Bernardo" (5, 5))
+
+== __Propriedades:__
+prop> pessoaNorte [] = Nothing
+prop> pessoaNorte [a] = Just a
+-}
+pessoaNorte :: [PosicaoPessoa] -> Maybe PosicaoPessoa
+pessoaNorte [] = Nothing
+pessoaNorte [a] = Just a
+pessoaNorte ((Pos name1 (x1, y1)) : (Pos name2 (x2, y2)) : t)
+  | y1 > y2 = pessoaNorte (Pos name1 (x1, y1) : t)
+  | otherwise = pessoaNorte (Pos name2 (x2, y2) : t)
+
+
+{-| A função 'pessoasNorte' recebe uma lista de posições de pessoas e indica quais as pessoas mais a Norte.
+Ou seja, uma lista com o nome de todas as pessoas, exceto a(s) que se encontre(m) mais a Sul.
+
+==  __Notas:__
+* Assume-se que a pessoa mais a Sul é aquela cuja ordenada é menor.
+* Caso exista apenas um elemento na lista, o resultado terá esse elemento.
+
+==  __Exemplos de utilização:__
+>>> pessoasNorte [(Pos "Afonso" (0, 0)), (Pos "Bernardo" (5, -5)), (Pos "Carlos" (5, -5))]
+["Afonso"]
+>>> pessoasNorte [(Pos "Afonso" (0, 0)), (Pos "Bernardo" (5, 5)), (Pos "Carlos" (5, -5))]
+["Bernardo", "Afonso"]
+
+== __Propriedades:__
+prop> pessoasNorte [] = []
+prop> pessoasNorte [Pos name (x, y)] = [name]
+-}
+pessoasNorte :: [PosicaoPessoa] -> [String]
+pessoasNorte [] = [] 
+pessoasNorte [Pos name _] = []
+pessoasNorte ((Pos name1 (x1, y1)) : (Pos name2 (x2, y2)) : t)
+  | y1 < y2 = name2 : pessoasNorte (Pos name1 (x1, y1) : t)
+  | y1 > y2 = name1 : pessoasNorte (Pos name2 (x2, y2) : t)
+  | otherwise = pessoasNorte (Pos name1 (x1, y1) : t)
